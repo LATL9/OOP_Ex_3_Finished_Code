527aa00fe444bc084ff64c02650a162ba6f06a6d
LATL9 <113098926+LATL9@users.noreply.github.com>
2022-12-12 10:21:49 +0000

    added above code to existing code

---
+++ main.py
@@ -0,0 +1,7 @@
+def __init__(self,description,price,inventory):
+    self.__description = description
+    self.__price = price
+    self.__inventory = inventory
+
+Product1 = Product("parsnip",0.5,12)
+Product2 = Product("cucumber",0.75,16)
